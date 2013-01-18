9a2dbbdf4c547f2ff700c235fcf2f0114d515e99
datastream <guxianjie@gmail.com>
2013-01-18 11:36:28 +0800

    update testcase

--- btree_test.go
+++ btree_test.go
@@ -6,7 +6,9 @@ import (
 	"testing"
 )
 
-func testBtreeInsert(t *testing.T, tree *btree.Btree, size int) {
+func TestInsert(t *testing.T) {
+	tree := btree.NewBtreeSize(2,2)
+	size := 100
 	rst := make(chan bool)
 	for i := 0; i < size; i++ {
 		rd := &btree.Record{
@@ -20,17 +22,39 @@ func testBtreeInsert(t *testing.T, tree *btree.Btree, size int) {
 		}
 	}
 }
-func testBtreeSearch(t *testing.T, tree *btree.Btree, size int) {
+func TestSearch(t *testing.T) {
+	tree := btree.NewBtreeSize(2,2)
+	size := 100
+	rst := make(chan bool)
+	for i := 0; i < size; i++ {
+		rd := &btree.Record{
+			Key:   []byte(strconv.Itoa(i)),
+			Value: []byte(strconv.Itoa(i)),
+		}
+		go tree.Insert(rd, rst)
+		<- rst
+	}
 	for i := 0; i < size; i++ {
 		q_rst := make(chan []byte)
 		go tree.Search([]byte(strconv.Itoa(i)), q_rst)
 		rst := <-q_rst
-		if rst == nil {
+		if string(rst) != strconv.Itoa(i) {
 			t.Fatal("Find Failed", i)
 		}
 	}
 }
-func testBtreeUpdate(t *testing.T, tree *btree.Btree, size int) {
+func TestUpdate(t *testing.T) {
+	tree := btree.NewBtreeSize(2,2)
+	size := 100
+	rst := make(chan bool)
+	for i := 0; i < size; i++ {
+		rd := &btree.Record{
+			Key:   []byte(strconv.Itoa(i)),
+			Value: []byte(strconv.Itoa(i)),
+		}
+		go tree.Insert(rd, rst)
+		<- rst
+	}
 	for i := 0; i < size; i++ {
 		rd := &btree.Record{
 			Key:   []byte(strconv.Itoa(i)),
@@ -43,30 +67,27 @@ func testBtreeUpdate(t *testing.T, tree *btree.Btree, size int) {
 			t.Fatal("Update Failed", i)
 		}
 	}
-}
-func testBtreeDeleteCheck(t *testing.T, tree *btree.Btree, size int) {
 	for i := 0; i < size; i++ {
 		q_rst := make(chan []byte)
 		go tree.Search([]byte(strconv.Itoa(i)), q_rst)
 		rst := <-q_rst
-		if rst == nil {
+		if string(rst) != strconv.Itoa(i+1) {
 			t.Fatal("Find Failed", i)
-		}
-		d_rst := make(chan bool)
-		go tree.Delete([]byte(strconv.Itoa(i)), d_rst)
-		stat := <-d_rst
-		q_rst = make(chan []byte)
-		go tree.Search([]byte(strconv.Itoa(i)), q_rst)
-		rst = <-q_rst
-		if rst != nil {
-			t.Fatal("Find deleted key", i)
-		}
-		if !stat {
-			t.Fatal("delete Failed", i)
 		}
 	}
 }
-func testBtreeDelete(t *testing.T, tree *btree.Btree, size int) {
+func TestDelete(t *testing.T) {
+	tree := btree.NewBtreeSize(2,2)
+	size := 100
+	rst := make(chan bool)
+	for i := 0; i < size; i++ {
+		rd := &btree.Record{
+			Key:   []byte(strconv.Itoa(i)),
+			Value: []byte(strconv.Itoa(i)),
+		}
+		go tree.Insert(rd, rst)
+		<- rst
+	}
 	for i := 0; i < size; i++ {
 		d_rst := make(chan bool)
 		go tree.Delete([]byte(strconv.Itoa(i)), d_rst)
@@ -74,23 +95,6 @@ func testBtreeDelete(t *testing.T, tree *btree.Btree, size int) {
 		if !stat {
 			t.Fatal("delete Failed", i)
 		}
-	}
-}
-
-func TestBtree(t *testing.T) {
-	tree := btree.NewBtree()
-	size := 100000
-	testBtreeInsert(t, tree, size)
-	testBtreeSearch(t, tree, size)
-	testBtreeUpdate(t, tree, size)
-	testBtreeSearch(t, tree, size)
-	testBtreeDeleteCheck(t, tree, size)
-	testBtreeInsert(t, tree, size)
-	tree.Dump("treedump")
-	if ntree, err := btree.Restore("treedump_0"); err == nil {
-		testBtreeSearch(t, ntree, size)
-		testBtreeUpdate(t, ntree, size)
-		testBtreeDelete(t, ntree, size)
 	}
 }
 
